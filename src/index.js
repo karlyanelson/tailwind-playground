6949d9fc4996378f8f83ac53c1b92c53667b1ba9
Karly Nelson <magnet-dev+kn@asynchrony.com>
2020-11-12 13:25:25 -0600

    remove index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("click", function (e) {
-  if (!e.target.matches("button")) {
-    return;
-  }
-
-  if (e.target.matches("[data-toggle]")) {
-    document.body.classList.toggle("dark");
-  }
-});
